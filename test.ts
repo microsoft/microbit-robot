cae3aaf980c0b5e70cfa7c925092b95c0de8eefb
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-12-05 22:10:00 -0800

    updated default robot to elecfreaks

--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
-//robot.elecfreaksCuteBot.start()
+robot.elecfreaksCuteBot.start()
 //robot.yahboomTinyBit.start(
 //robot.elecfreaksCuteBotPro.start()
 //robot.keyStudioMiniSmartRobot.start()
 //robot.setMotorDrift(6)
-robot.dfRobotMaqueen.start()
+//robot.dfRobotMaqueen.start()
 //robot.dfRobotMaqueenPlusV2.start()
 //robot.kittenbotMiniLFR.start()
 //robot.inksmithK8.start()
