ef94be374b35de88aad6934eaa39d6491d283d45
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-05-07 04:53:38 +0000

    document maqueen

--- robots/dfrobotmaqueen.ts
+++ robots/dfrobotmaqueen.ts
@@ -1,18 +1,12 @@
 namespace robot {
+    // https://github.com/DFRobot/pxt-maqueen/blob/master/maqueen.ts#L20
     const I2C_ADRESS = 0x10
+    // https://github.com/DFRobot/pxt-maqueen/blob/master/maqueen.ts#L46
     const M1_INDEX = 0
     const M2_INDEX = 0x02
+    // https://github.com/DFRobot/pxt-maqueen/blob/master/maqueen.ts#L62
     const FORWARD = 0
     const BACKWARD = 1
-    function run(index: number, speed: number): void {
-        const buf = pins.createBuffer(3)
-        const direction = speed > 0 ? FORWARD : BACKWARD
-        const s = Math.round(Math.map(Math.abs(speed), 0, 100, 0, 255))
-        buf[0] = index
-        buf[1] = direction
-        buf[2] = s
-        pins.i2cWriteBuffer(I2C_ADRESS, buf)
-    }
 
     // https://github.com/DFRobot/pxt-maqueen/blob/master/maqueen.ts
     class DFRobotMaqueenRobot extends robots.Robot {
@@ -27,9 +21,20 @@ namespace robot {
             this.sonar = new drivers.SR04Sonar(DigitalPin.P2, DigitalPin.P1)
         }
 
+        private run(index: number, speed: number): void {
+            // https://github.com/DFRobot/pxt-maqueen/blob/master/maqueen.ts#L46            
+            const buf = pins.createBuffer(3)
+            const direction = speed > 0 ? FORWARD : BACKWARD
+            const s = Math.round(Math.map(Math.abs(speed), 0, 100, 0, 255))
+            buf[0] = index
+            buf[1] = direction
+            buf[2] = s
+            pins.i2cWriteBuffer(I2C_ADRESS, buf)
+        }
+    
         motorRun(left: number, right: number): void {
-            run(M1_INDEX, left)
-            run(M2_INDEX, right)
+            this.run(M1_INDEX, left)
+            this.run(M2_INDEX, right)
         }
 
         headlightsSetColor(red: number, green: number, blue: number): void {
