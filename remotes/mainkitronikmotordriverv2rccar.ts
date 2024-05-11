71bd84a8a579008df43639763643aa6e32ac45c8
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-05-11 15:42:18 +0000

    fix remote

--- remotes/mainkitronikmotordriverv2rccar.ts
+++ remotes/mainkitronikmotordriverv2rccar.ts
@@ -1,12 +1,3 @@
 robot.kitronikMotorDriverV2RCCar.start()
 robot.startCompactRadio()
 robot.startCalibrationButtons()
-input.onButtonPressed(Button.A, () => {
-    basic.pause(1000)
-    robot.motorTank(50, 50, 3000)
-    robot.motorStop()
-    robot.motorTank(100, 0, 3000)
-    robot.motorStop()
-    robot.motorTank(0, 100, 3000)
-    robot.motorStop()
-})
